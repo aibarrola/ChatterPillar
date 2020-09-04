fa3f03e6e46cc5e3705cbce1b2e57c99cc151a03
Angelo Ibarrola <vino_1105@hotmail.com>
2020-09-04 15:31:19 -0700

    added basic routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './App.css';
+import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom'
+
+import SignUp from './components/pages/signUp'
+import SignIn from './components/pages/signIn'
+import HomePage from './components/pages/homePage'
+import NotFound from './components/pages/notFound'
+
 
 function App() {
   return (
     <div className="App">
-      <h1> Landing </h1>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={SignUp} />
+          <Route path="/signin" component={SignIn} />
+          <Route path="/home" component={HomePage} />
+          <Route component={NotFound} />
+        </Switch>
+      </Router>
     </div>
   );
 }
