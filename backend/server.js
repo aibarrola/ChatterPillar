b542a97573c785593413a3452155a17272b139bd
thoughtless02 <62426189+thoughtless02@users.noreply.github.com>
2020-09-05 20:00:46 +0530

    Fixed server file to include chat

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
 require('dotenv').config();
-
 const app = express();
 const port = process.env.PORT || 5000;
-
 app.use(cors());
 app.use(express.json());
+
+
+const http = require('http');
+const socketio = require('socket.io');
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./chat-users');
+const server = http.createServer(app);
+const io = socketio(server);
+
+
+
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
@@ -20,9 +27,51 @@ connection.once('open', () =>{
 
 const usersRouter = require('./routes/users');
 const postsRouter = require('./routes/posts');
-
 app.use('/posts',postsRouter);
 app.use('/users', usersRouter);
+
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
+});
+
+
+
+
+
+
+
+
+io.on('connect', (socket) => {
+  socket.on('join', ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
+    console.log("connected");
+
+    if(error) return callback(error);
+
+    socket.join(user.room);
+
+    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.`});
+    socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
+
+    io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+
+    callback();
+  });
+
+  socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit('message', { user: user.name, text: message });
+
+    callback();
+  });
+
+  socket.on('disconnect', () => {
+    const user = removeUser(socket.id);
+
+    if(user) {
+      io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
+      io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+    }
+  })
 });
